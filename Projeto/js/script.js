e80d8f3ae13371186ba088a694004df2f0a3930b
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-04-09 01:08:36 -0300

    Nova Versão Completa 0.3
    adicionei troca de perfil controle de volumes volume mais menos pausar muta background muda conforme o perfil

--- Projeto/js/script.js
+++ Projeto/js/script.js
@@ -12,3 +12,84 @@ playPause.addEventListener("click", () => {
       playPause.querySelector(".play-btn").classList.toggle("hide");
     }
   });
+    
+  function changeColor(color){
+    const fundo = document.querySelector(body);
+    fundo.style.background = color;
+    }
+
+  function imgSlider(anything){
+    document.querySelector('.player-img').src = anything;
+  }
+
+  function menuimg(){
+    const toggleMenu = document.querySelector('.player2');
+    const navigation = document.querySelector('.painel');
+    const one = document.querySelector('.one');
+    const two = document.querySelector('.two');
+    const three = document.querySelector('.three');
+    toggleMenu.classList.toggle('ativo');
+    navigation.classList.toggle('ativo');
+    one.classList.toggle('ativo');
+    two.classList.toggle('ativo');
+    three.classList.toggle('ativo');
+  }
+
+  function sair(){
+    const toggleMenu = document.querySelector('.player2');
+    const navigation = document.querySelector('.painel');
+    const one = document.querySelector('.one');
+    const two = document.querySelector('.two');
+    const three = document.querySelector('.three');
+    toggleMenu.classList.remove('ativo');
+    navigation.classList.remove('ativo');
+    one.classList.remove('ativo');
+    two.classList.remove('ativo');
+    three.classList.remove('ativo');
+  }
+
+  function volume(){
+    const painelControles = document.querySelector('.painelControles');
+    const center = document.querySelector('.center');
+    const volume1 = document.querySelector('.volume1');
+    const volume2 = document.querySelector('.volume2');
+    const volume3 = document.querySelector('.volume3');
+    volume1.classList.toggle('ativo');
+    volume2.classList.toggle('ativo');
+    volume3.classList.toggle('ativo');
+    painelControles.classList.toggle('ativo');
+    center.classList.toggle('ativo');
+  }
+  function backgroundNormal(){
+    const container = document.querySelector('.container');
+    container.classList.remove('aqua');
+    container.classList.remove('red');
+    container.classList.remove('blue');
+  }
+
+  function backgroundFundo1(){
+    const background = document.querySelector('.container');
+    background.classList.toggle('aqua');
+  }
+
+  function backgroundFundo2(){
+    const background = document.querySelector('.container');
+    background.classList.toggle('red');
+  }
+
+  function backgroundFundo3(){
+    const background = document.querySelector('.container');
+    background.classList.toggle('blue');
+  }
+
+  function voltarInicio(){
+    location = "index.html";
+  }
+    
+
+  
+  
+
+
+    
+    
---
+++ Projeto/js/scriptcontrols.js
@@ -0,0 +1,122 @@
+var i = 0;
+    var musicas =   [
+            {mp3:'som/All TikTok Mashup [ JVKE - Upside Down ] _ Buttercup x Hood Baby TikTok Remix.mp3', ogg:'the_godfather_main_title.ogg',
+			 titulo:'Upside Down',
+			  autor:'Remix TikTok'},
+
+			{mp3:'som/Twenty_Carneyval Mashup_TikTok Mashup.mp3',
+			 ogg:'the_godfather_main_title.ogg',
+			 titulo:'Twenty_Carneyval',
+			  autor:'TikTok'},
+
+			{mp3:'som/30 deep grimeyy -Rockstar.mp3',
+			 ogg:'the_godfather_main_title.ogg',
+			  titulo:'30 deep grimeyy', 
+			  autor:'Rockstar'},
+ 
+
+			{mp3:'som/Xenial - Lost  2.mp3',
+			 ogg:'the_godfather_main_title.ogg',
+			  titulo:'Lost', 
+			  autor:'Xenial'},
+
+			{mp3:'som/Ellit - Sad and Sober.mp3',
+			 ogg:'the_godfather_main_title.ogg',
+			  titulo:'Sad and Sober',
+			   autor:'Ellit-Sad'},
+
+			   {mp3:'som/Capital Cities  Safe And Sound  TraduoLegendado.mp3',
+			 ogg:'the_godfather_main_title.ogg',
+			  titulo:'Capital Cities',
+			   autor:'Safe And Sound'}
+
+            ]; 
+
+    audio = document.getElementById('audio'); 
+
+    audio.addEventListener('canplay', play_evento , false);
+    audio.addEventListener('timeupdate', atualizar , false);
+    audio.addEventListener('ended', proxima , false);
+
+	function aumentar_volume(){
+				if( audio.volume < 1)  audio.volume += 0.1;
+			}
+
+			function diminuir_volume(){
+				if( audio.volume > 0)  audio.volume -= 0.1;
+			}
+			
+			function mute(){
+				if( audio.muted ){
+					audio.muted = false;
+				}else{
+					audio.muted = true;
+				}
+			}
+ 
+    function play(){
+        proxima();
+    }
+ 
+    function pause(){
+        audio.pause();
+    }
+ 
+    function stop(){
+        audio.pause();
+        audio.currentTime = 0;
+    }
+ 
+    function play_evento(){
+        document.getElementById('tempo_atual').innerHTML = secToStr( audio.currentTime) ;
+        document.getElementById('tempo_total').innerHTML = secToStr( audio.duration );
+        document.getElementById('barra_progresso').max = audio.duration;
+        document.getElementById('barra_progresso').value = audio.currentTime;
+    }
+ 
+    function atualizar(){
+        document.getElementById('tempo_atual').innerHTML = secToStr( audio.currentTime);
+        document.getElementById('barra_progresso').value = audio.currentTime;
+    }
+ 
+    function proxima(){
+        if(audio.canPlayType("audio/mp3") != ''){
+            audio.src = musicas[i].mp3;
+        }else{
+            audio.src = musicas[i].ogg;
+        }
+        document.getElementById('nome_musica').innerHTML = musicas[i].titulo;
+		document.getElementById('nome_autor').innerHTML = musicas[i].autor;
+        audio.play();
+ 
+        i++;
+        if( i >= musicas.length ) i = 0;
+    }
+
+	function voltar(){
+        if(audio.canPlayType("audio/mp3") != ''){
+            audio.src = musicas[i].mp3;
+        }else{
+            audio.src = musicas[i].ogg;
+        }
+        document.getElementById('nome_musica').innerHTML = musicas[i].titulo;
+		document.getElementById('nome_autor').innerHTML = musicas[i].autor;
+        audio.play();
+        
+        --i;
+        if( i >= musicas.length ) i = 0;
+    }
+
+ 
+    function secToStr( sec_num ) {
+        sec_num = Math.floor( sec_num );
+        var horas   = Math.floor(sec_num / 3600);
+        var minutos = Math.floor((sec_num - (horas * 3600)) / 60);
+        var segundos = sec_num - (horas * 3600) - (minutos * 60);
+ 
+        if (horas   < 10) {horas   = "0"+horas;}
+        if (minutos < 10) {minutos = "0"+minutos;}
+        if (segundos < 10) {segundos = "0"+segundos;}
+        var tempo    = horas+':'+minutos+':'+segundos;
+        return tempo;
+    }
